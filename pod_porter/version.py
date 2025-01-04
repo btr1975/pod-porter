f8a56566b990661b9572e1212d2fe268ffcffae9
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2025-01-04 16:17:10 -0600

    Updated copyright, and changed version to 0.1.3

--- pod_porter/version.py
+++ pod_porter/version.py
@@ -2,8 +2,8 @@
 Holds the version information for the package
 """
 
-__copyright__ = "Copyright (c) 2024 Benjamin P. Trachtenberg"
+__copyright__ = "Copyright (c) 2024-2025 Benjamin P. Trachtenberg"
 __status__ = "prod"
 
-__version_info__ = (0, 1, 2)
+__version_info__ = (0, 1, 3)
 __version__ = ".".join(map(str, __version_info__))
