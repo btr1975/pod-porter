2f82c02024884355d48c3a7ce4a43aca7a77dbc8
Ben Trachtenberg <btr1975@gmail.com>
2025-04-27 09:59:16 -0500

    updated for uv

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
 # Makefile for project needs
 # Author: Ben Trachtenberg
-# Version: 1.0.8
+# Version: 2.0.0
 #
 
 .PHONY: all info build build-container coverage format pylint pytest start-container stop-container remove-container \
-        gh-pages check-vuln
+        gh-pages check-security pip-export
 
 info:
 	@echo "make options"
 	@echo "    all                 To run coverage, format, pylint, and check-vuln"
 	@echo "    build               To build a distribution"
 	@echo "    build-container     To build a container image"
-	@echo "    check-vuln          To check for vulnerabilities in the dependencies"
 	@echo "    coverage            To run coverage and display ASCII and output to htmlcov"
 	@echo "    format              To format the code with black"
 	@echo "    pylint              To run pylint"
@@ -21,33 +20,33 @@ info:
 	@echo "    remove-container    To remove the container"
 
 
-all: coverage format pylint check-vuln
+all: format pylint coverage check-security pip-export
 
 build:
-	@python -m build
+	@uv build --wheel --sdist
 
+coverage:
+	@uv run pytest --cov --cov-report=html -vvv
+
+format:
+	@uv run black pod_porter/
+	@uv run black tests/
+
+pylint:
+	@uv run pylint pod_porter/
+
+pytest:
+	@uv run pytest --cov -vvv
+
+check-security:
+	@uv run bandit -c pyproject.toml -r .
+
+pip-export:
+	@uv export --no-dev --no-emit-project --no-editable > requirements.txt
+	@uv export --no-emit-project --no-editable > requirements-dev.txt
 
 build-container:
 	@cd containers && podman build --ssh=default --build-arg=build_branch=main -t pod-porter:latest -f Containerfile
-
-
-
-
-coverage:
-	@pytest --cov --cov-report=html -vvv
-
-format:
-	@black pod_porter/
-	@black tests/
-
-pylint:
-	@pylint pod_porter/
-
-pytest:
-	@pytest --cov -vvv
-
-
-
 
 start-container:
 	@podman run -itd --name pod-porter -p 8080:8080 localhost/pod-porter:latest
@@ -58,11 +57,7 @@ stop-container:
 remove-container:
 	@podman rm pod-porter
 
-
 gh-pages:
 	@rm -rf ./docs/source/code
 	@sphinx-apidoc -o ./docs/source/code ./pod_porter
 	@sphinx-build ./docs ./docs/gh-pages
-
-check-vuln:
-	@pip-audit -r requirements.txt
--- make.bat
+++ make.bat
@@ -1,53 +1,61 @@
 @ECHO OFF
 REM Makefile for project needs
 REM Author: Ben Trachtenberg
-REM Version: 1.0.7
+REM Version: 2.0.0
 REM
 
 IF "%1" == "all" (
-    pytest --cov --cov-report=html -vvv
-    black pod_porter/
-    black tests/
-    pylint pod_porter\
-    pip-audit -r requirements.txt
+    uv run black pod_porter/
+    uv run black tests/
+    uv run pylint pod_porter\
+    uv run pytest --cov --cov-report=html -vvv
+    uv run bandit -c pyproject.toml -r .
+    uv export --no-dev --no-emit-project --no-editable > requirements.txt
+    uv export --no-emit-project --no-editable > requirements-dev.txt
     GOTO END
 )
 
 IF "%1" == "build" (
-    python -m build
+    uv build --wheel --sdist
     GOTO END
 )
 
 IF "%1" == "coverage" (
-    pytest --cov --cov-report=html -vvv
+    uv run pytest --cov --cov-report=html -vvv
     GOTO END
 )
 
 IF "%1" == "pylint" (
-    pylint pod_porter\
+    uv run pylint pod_porter\
     GOTO END
 )
 
 IF "%1" == "pytest" (
-    pytest --cov -vvv
+    uv run pytest --cov -vvv
     GOTO END
 )
 
 IF "%1" == "format" (
-    black pod_porter/
-    black tests/
+    uv run black pod_porter/
+    uv run black tests/
     GOTO END
 )
 
-IF "%1" == "check-vuln" (
-    pip-audit -r requirements.txt
+IF "%1" == "check-security" (
+    uv run bandit -c pyproject.toml -r .
+    GOTO END
+)
+
+IF "%1" == "pip-export" (
+    uv export --no-dev --no-emit-project --no-editable > requirements.txt
+    uv export --no-emit-project --no-editable > requirements-dev.txt
     GOTO END
 )
 
 IF "%1" == "gh-pages" (
     rmdir /s /q docs\source\code
-    sphinx-apidoc -o ./docs/source/code ./pod_porter
-    sphinx-build ./docs ./docs/gh-pages
+    uv run sphinx-apidoc -o ./docs/source/code ./pod_porter
+    uv run sphinx-build ./docs ./docs/gh-pages
     GOTO END
 )
 
@@ -55,7 +63,6 @@ IF "%1" == "gh-pages" (
 @ECHO     all             To run coverage, format, pylint, and check-vuln
 @ECHO     build           To build a distribution
 @ECHO     coverage        To run coverage and display ASCII and output to htmlcov
-@ECHO     check-vuln      To check for vulnerabilities in the dependencies
 @ECHO     format          To format the code with black
 @ECHO     pylint          To run pylint
 @ECHO     pytest          To run pytest with verbose option
